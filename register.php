3ff3d9cd1541d92f521dfa84b353f88b9cd72f93
junxian.yong <junxian.yong@gmail.com>
2016-09-24 03:42:40 -0400

    reverse

--- register.php
+++ register.php
@@ -9,7 +9,7 @@ $db_user = getenv('DB_USER');
 $db_pass = getenv('DB_PASS');
 
 $dbh = new PDO('sqlsrv:server = ' . $db_host . '; Database = ' . $db_name, $db_user, $db_pass);
-$stmt = $dbh->prepare("SELECT * FROM users where username = ?");
+$stmt = $dbh->prepare("SELECT * FROM `users` where `username` = ?");
 $stmt->execute([$username]);
 
 
@@ -19,7 +19,7 @@ if($stmt->rowCount()) {
 } else {
 	// Username not taken
     // So we INSERT into database
-    $stmt2 = $dbh->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
+    $stmt2 = $dbh->prepare("INSERT INTO `users` (username, password) VALUES (?, ?)");
     $stmt2->execute([$username, $password]);
     echo 'User registered successfully!';
 }
